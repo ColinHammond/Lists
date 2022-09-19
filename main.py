ec9fc365e3d76271311952897203c83d46a20fce
ColinHammond <c2hammond@student.bridgew.edu>
2022-09-19 19:48:25 -0400

    Learning Lists

---
+++ main.py
@@ -0,0 +1,5 @@
+stations = ["Everus", "Dream", "HUR L1", "HUR L2", "Orison", "Hurston", "ArcCorp", "MircoTech"]
+stations.append("Dream")
+stations.remove("ArcCorp")
+stations.pop(-3)
+print(stations)
